ccfda319d4cdaa52efeda8ab3698040498c54029
vagrant <vagrant@homestead>
2018-07-20 04:05:42 +0000

    Notify user in email with Queue supported

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use App\Models\Reply;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -20,10 +20,10 @@ class TopicReplied extends Notification
         $this->reply = $reply;
     }
 
-    public function via($notifiable)
+   public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     public function toDatabase($notifiable)
@@ -42,5 +42,13 @@ class TopicReplied extends Notification
             'topic_id' => $topic->id,
             'topic_title' => $topic->title,
         ];
+    }
+     public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
+        return (new MailMessage)
+                    ->line('你的话题有新回复！')
+                    ->action('查看回复', $url);
     }
 }
